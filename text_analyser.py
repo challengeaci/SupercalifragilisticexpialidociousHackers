a4756093c5ddbcc4ba9dd2c4adcca729e4bd0c6e
akshat <akshatbhargavadeepa@gmail.com>
2018-10-14 00:28:55 +0530

    Initial Commit

---
+++ text_analyser.py
@@ -0,0 +1,10 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import json
+
+sid = SentimentIntensityAnalyzer()
+
+def analyse_text(message_text):
+  scores = sid.polarity_scores(message_text)
+  return json.dumps(scores)
+
+# print(analyse_text('hi how are you'));
